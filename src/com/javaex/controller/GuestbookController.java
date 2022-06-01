6651a1652240aeeea78dceab4dd5c75112173418
midgjs <sjqksk.10@gmail.com>
2022-06-01 22:59:19 +0900

    게스트북2

--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -14,101 +14,58 @@ import com.javaex.dao.GuestbookDao;
 import com.javaex.util.WebUtil;
 import com.javaex.vo.GuestbookVo;
 
-
 @WebServlet("/gbc")
 public class GuestbookController extends HttpServlet {
-	//필드
 	private static final long serialVersionUID = 1L;
-    
-	//생성자(default)
-	
-	//메소드 gs
-	
-	//메소드 일반
-	//get방식으로 요청시 호출 메서드
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//코드작성
-		System.out.println("controller");
-		
-		
-		//포스트 방식일때 한글깨짐 방지
-		request.setCharacterEncoding("UTF-8");
-		
-		//action파라미터 꺼내기
-		String action = request.getParameter("action");
-		System.out.println(action);
-		
-		
-		if("addList".equals(action)) { //리스트일때
-			//데이터 가져오기
-			GuestbookDao gDao = new GuestbookDao();
-			List<GuestbookVo> gList = gDao.getList();
-			System.out.println(gList);
-			
-			//request에 데이터 추가
-			request.setAttribute("gList", gList);
-			
-			//데이터 + html --> jsp 시킨다
-			WebUtil.forward(request, response, "/WEB-INF/addList.jsp");
-			
-			/*
-			RequestDispatcher rd = request.getRequestDispatcher("/gList.jsp");
-			rd.forward(request, response);
-			*/
-		}else if("add".equals(action)) { //등록일때
 
-			//파라미터에서 값 꺼내기(name, hp ,company)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		String action = request.getParameter("action");
+
+		if ("add".equals(action)) {
+
 			String name = request.getParameter("name");
-			String hp = request.getParameter("hp");
-			String company = request.getParameter("company");
+			String password = request.getParameter("pass");
+			String content = request.getParameter("content");
+
+			GuestbookDao dao = new GuestbookDao();
+			GuestbookVo vo = new GuestbookVo(name, password, content);
+			dao.insert(vo);
+			System.out.println(vo.toString());
+
+			WebUtil.redirect(request, response, "/guestbook2/gbc");
+
+		} else if ("deleteform".equals(action)) {
+			WebUtil.forword(request, response, "/WEB-INF/deleteForm.jsp");
+
+		} else if ("delete".equals(action)) {
+			int no = Integer.parseInt(request.getParameter("no"));
+			String password = request.getParameter("pass");
+
+			GuestbookVo vo = new GuestbookVo();
+			vo.setNo(no);
+			vo.setPassword(password);
+
+			GuestbookDao dao = new GuestbookDao();
+			dao.delete(vo);
+
+			WebUtil.redirect(request, response, "/guestbook2/gbc");
 			
-			//vo만들어서 값 초기화
-			GuestbookVo guestbookVo = new GuestbookVo(name, hp, company);
-			System.out.println(guestbookVo);
-			
-			
-			GuestbookDao gDao = new GuestbookDao();
-			int count = gDao.guestbookInsert(guestbookVo);
-			System.out.println(count);
-			
-			//리다이렉트 list
-			WebUtil.redirect(request, response, "./gbc?action=list");
-			/*
-			response.sendRedirect("./gbc?action=list");
-			*/
-			
-		}else if("delete".equals(action)) { //삭제일때
-			
-			//파라미터에서 id값을 꺼낸다
-			int id = Integer.parseInt(request.getParameter("id"));
-			
-			//phoneDao.personDelete()를 통해 삭제하기
-			GuestbookDao guestbookDao = new GuestbookDao();
-			int count = gDao.guestbookDelete(id);
-			
-			//리다이렉트 list
-			WebUtil.redirect(request, response, "./gbc?action=list");
-			/*
-			response.sendRedirect("./gbc?action=list");
-			*/
-			
-		}else if("deleteform".equals(action)) { //삭제폼
-			int count = Integer.parseInt(request.getParameter("count"));
-			
-			request.setAttribute("count", count);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/deleteForm.jsp");
+		} else {//리스트
+			GuestbookDao dao = new GuestbookDao();
+			List<GuestbookVo> gList = dao.getList();
+
+			request.setAttribute("guestList", gList);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/addList.jsp");
 			rd.forward(request, response);
-		}else {
-			System.out.println("action 파라미터 없음");
 		}
-		
+
 	}
-	
-	//post방식으로 요청시 호출 메소드
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("여기는 post");
-		
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
--- src/com/javaex/dao/GuestbookDao.java
+++ src/com/javaex/dao/GuestbookDao.java
@@ -12,33 +12,75 @@ import com.javaex.vo.GuestbookVo;
 
 public class GuestbookDao {
 
-	public List<GuestbookVo> getList() {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		List<GuestbookVo> list = new ArrayList<GuestbookVo>();
+	// 0. import java.sql.*;
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+
+	private void getConnection() {
 		try {
 			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName(driver);
 
 			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			conn = DriverManager.getConnection(url, id, pw);
+			// System.out.println("접속성공");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	private void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	public List<GuestbookVo> getList() {
+		List<GuestbookVo> list = new ArrayList<GuestbookVo>();
+
+		this.getConnection();
+
+		try {
 
 			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = " select no, " + 
-						   "        name, " + 
-					       "        password, " + 
-					       "        content, " + 
-					       "        reg_date " + 
-			               " from guestbook " +
-					       " order by reg_date desc ";
+
+			// SQL문 준비
+			String query = "";
+			query += " select no, ";
+			query += "        name, ";
+			query += "        password, ";
+			query += "        content, ";
+			query += "        reg_date ";
+			query += " from guestbook ";
+			query += " order by reg_date desc  ";
+
+			// 바인딩
 			pstmt = conn.prepareStatement(query);
-			System.out.println("=====");
 			System.out.println(query);
+
+			// 실행
 			rs = pstmt.executeQuery();
+
 			// 4.결과처리
 			while (rs.next()) {
 				int no = rs.getInt("no");
@@ -51,109 +93,68 @@ public class GuestbookDao {
 				list.add(vo);
 			}
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-
 		}
+
+		this.close();
 
 		return list;
 	}
 
 	public int insert(GuestbookVo vo) {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
 		int count = 0;
 
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+		this.getConnection();
 
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		try {
 
 			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = " insert into guestbook " + 
-			               "        (no, " + 
-			               "         name, " +
-			               "         password, " +
-			               "         content, " +
-			               "         reg_date) " +
-			               " values (seq_guestbook_no.nextval, " +
-			               "         ?, " +
-			               "         ?, " +
-			               "         ?, " +
-			               "         sysdate) " ;	
-			
+			// SQL문 준비
+			String query = "";
+			query += " insert into guestbook (no, name, password, content, reg_date) ";
+			query += " values (seq_guestbook_no.nextval, ?, ?, ?, sysdate) ";
+
+			// 바인딩
 			pstmt = conn.prepareStatement(query);
 
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getPassword());
 			pstmt.setString(3, vo.getContent());
 
+			// 실행
 			count = pstmt.executeUpdate();
 
 			// 4.결과처리
 			System.out.println(count + "건 등록");
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-
 		}
+
+		this.close();
 
 		return count;
 	}
 
 	public int delete(GuestbookVo vo) {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
 		int count = 0;
 
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+		this.getConnection();
 
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		try {
 
 			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = " delete from guestbook " + 
-					       " where no= ? " + 
-				           " and password= ? " ;
-			
+			// SQL문 준비
+			String query = "";
+			query += " delete from guestbook ";
+			query += " where no= ?  ";
+			query += " and password= ?  ";
+
+			// 바인딩
 			pstmt = conn.prepareStatement(query);
 
+			// 실행
 			pstmt.setInt(1, vo.getNo());
 			pstmt.setString(2, vo.getPassword());
 
@@ -162,24 +163,11 @@ public class GuestbookDao {
 			// 4.결과처리
 			System.out.println(count + "건 삭제");
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-
 		}
+
+		this.close();
 
 		return count;
 	}
